68962cb8b80ee6a23ad630a43b3fed7ef6ba9f10
vjsujal <www.sujalvj121@gmail.com>
2023-11-28 18:58:41 +0530

    Update settings.py

--- Recommendation_System/settings.py
+++ Recommendation_System/settings.py
@@ -118,10 +118,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIRS = [
-    os.path.join(str(BASE_DIR.joinpath('static')),)
-] 
+
 
 
 # Define the upload folder path
